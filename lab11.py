17a81eaf8d5e149936f651383878892b95219315
kotkem <louishowk9@gmail.com>
2025-11-26 19:58:14 +0000

    lab11

---
+++ lab11.py
@@ -0,0 +1,47 @@
+# La siguiente variable muestra el codigo de la insulina
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+print(preproInsulin)
+
+
+# Almacena los elementos restantes de la secuencia de la insulina humana en variables:
+
+IsInsulin = "malwmrllpllallalwgpdpaaa"
+
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+
+aInsulin = "giveqcctsicslyqlenycn"
+
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
+
+insulin = bInsulin + aInsulin
+
+# imprimir secuencia de Insulina
+
+print("La secuencia de la preproinsulina humana:")
+
+print(preproInsulin)
+
+print("La secuencia de la Insulina Humana es, cadena: " + aInsulin)
+
+# Calculating the molecular weight of insulin  
+# Creating a list of the amino acid (AA) weights  
+aaWeights = {'A': 89.09, 'C': 121.16, 'D': 133.10, 'E': 147.13, 'F': 165.19,
+'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17, 'M': 149.21,
+'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20, 'S': 105.09, 'T': 119.12,
+'V': 117.15, 'W': 204.23, 'Y': 181.19}  
+# Count the number of each amino acids  
+aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A', 'C',
+'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+'V', 'W', 'Y']})  
+# Multiply the count by the weights  
+molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+'S', 'T', 'V', 'W', 'Y']}.values())  
+print("The rough molecular weight of insulin: " +
+str(molecularWeightInsulin))
+
+molecularWeightInsulinActual = 5807.63
+print("Error percentage: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+
